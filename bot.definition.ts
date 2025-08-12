a8e6fda0270f3e6d84bd459b1286bb6c9c173541
= <muhammadrehanbaig@gmail.com>
2025-08-12 23:25:03 +0500

    Bot as a code init

---
+++ bot.definition.ts
@@ -0,0 +1,6 @@
+import { BotDefinition } from '@botpress/sdk'
+import chat from './bp_modules/chat'
+export default new BotDefinition({}).addIntegration(chat,{
+    enabled:true,
+    configuration: {}
+})
