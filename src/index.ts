e3fe4f510c0e8ffc95309945af73a631bfecb00c
= <muhammadrehanbaig@gmail.com>
2025-08-12 23:40:02 +0500

    declarations

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
 import * as bp from '.botpress'
+import axios from 'axios'
 
+//declarations
+interface ConversionRates{
+  [currency:string] : number;
+}
+interface APIResponse{
+  result: string;
+  time_last_update_utc: string;
+  base_code: string;
+  conversion_rates: ConversionRates;
+}
 const bot = new bp.Bot({
   actions: {},
 })
